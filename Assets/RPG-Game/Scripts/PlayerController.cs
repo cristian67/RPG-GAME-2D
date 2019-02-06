4edb4cff12d0140f1073a8e71101026d47f3d7ca
Cristian <cristian.rock78@gmail.com>
2019-02-06 20:52:56 -0300

    Animacion de idle realizada

--- Assets/RPG-Game/Scripts/PlayerController.cs
+++ Assets/RPG-Game/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rgb2D;
     private Transform _transform;
 
+    //Movimiento
+    private Animator _anim;
+    private bool _isBack;
+    private SpriteRenderer _sprite;
+
     [SerializeField]
     private float _velocity;
 
@@ -17,15 +22,48 @@ public class PlayerController : MonoBehaviour
         inputPlayer = GetComponent<InputPlayer>();
         _transform  = GetComponent<Transform>();
         rgb2D       = GetComponent<Rigidbody2D>();
+        _anim       = GetComponent<Animator>();
+        _sprite     = GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
-    void Update()
-    { 
-
+    
+    void FixedUpdate()
+    {
         //Movement
         Vector2 _VelocityVector = new Vector2(inputPlayer._horizontal, inputPlayer._vertical) * _velocity;
-        //rgb2D.AddForce(force);
         rgb2D.velocity = _VelocityVector;
     }
+
+    void Update()
+    {
+
+        SplitSprite();
+
+        //Movimiento Vertical y horizontal
+        if (inputPlayer._horizontal != 0 || inputPlayer._vertical != 0)
+        {
+            SetXYAnimator();
+        }
+
+    }
+
+    private void SetXYAnimator()
+    {
+        _anim.SetFloat("X", inputPlayer._horizontal);
+        _anim.SetFloat("Y", inputPlayer._vertical);
+    }
+
+    private void SplitSprite()
+    {
+        // Flip del sprite
+        if (inputPlayer._horizontal > 0 && Mathf.Abs(inputPlayer._vertical) < Mathf.Abs(inputPlayer._horizontal))
+        {
+            _sprite.flipX = true;
+        }
+        else if (inputPlayer._horizontal != 0)
+        {
+            _sprite.flipX = false;
+        }
+    }
+
+  
 }
